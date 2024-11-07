114969d697c3a6be5a0e555efacc397cd1497c34
Patryk Nowak <patryk.nowak2@deutschepost.de>
2024-11-07 11:22:17 +0100

    Fix - now input file name is first command line argument.

--- src/main/java/org/nowpat/springbootedu/Runner.java
+++ src/main/java/org/nowpat/springbootedu/Runner.java
@@ -2,6 +2,7 @@ package org.nowpat.springbootedu;
 
 import java.util.Arrays;
 import org.nowpat.springbootedu.processor.Processor;
+import org.nowpat.springbootedu.reader.Reader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,11 @@ public class Runner implements CommandLineRunner {
     @Autowired
     private Processor processor;
 
+    private Reader reader = new Reader();
+
     @Override
     public void run(String... args) {
-        processor.process(Arrays.asList(args));
+
+        processor.process(reader.readFile(Arrays.asList(args).get(0)));
     }
 }
